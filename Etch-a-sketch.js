d066d268856e7d0f3245411a2d0b9e40d5aee35a
BennyCwp <87557594+BennyCwp@users.noreply.github.com>
2021-09-11 18:44:41 +0800

    Add files via upload

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -3,19 +3,20 @@ const section = document.querySelector(".section");
 const grayBtn = document.createElement("button");
 const resetBtn = document.createElement("button");
 const rainbowBtn = document.createElement("button");
+const btnContainer = document.querySelector(".btn-container");
 
 rainbowBtn.innerText = "Rainbow";
 grayBtn.innerText = "Gray";
 resetBtn.innerText = "Reset";
 
-section.appendChild(resetBtn);
-section.appendChild(grayBtn);
-section.appendChild(rainbowBtn);
+btnContainer.appendChild(resetBtn);
+btnContainer.appendChild(grayBtn);
+btnContainer.appendChild(rainbowBtn);
 
 
 
-function createGrid(cols, rows){
-    for (let i = 0; i < (cols * rows); i++){
+function createGrid(cols, rows) {
+    for (let i = 0; i < (cols * rows); i++) {
         let box = document.createElement("div");
         // box.classList.add("box");
         container.appendChild(box).classList.add("box");
@@ -23,14 +24,14 @@ function createGrid(cols, rows){
         container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     }
-    
+
 }
 
-function reset(){
+function reset() {
     const boxes = document.querySelectorAll(".box");
     boxes.forEach(box => box.remove());
     const userInput = prompt("Enter the number of grids, maximum: 100", 100);
-    if (userInput === null || userInput < 1){
+    if (userInput === null || userInput < 1) {
         createGrid(16, 16);
         grayColor();
     } else {
@@ -39,23 +40,23 @@ function reset(){
     }
 }
 
-function grayColor(){
+function grayColor() {
     const boxes = document.querySelectorAll(".box");
-    
-    boxes.forEach(box => box.addEventListener("mouseover",() => {
+
+    boxes.forEach(box => box.addEventListener("mouseover", () => {
         const random = Math.floor(Math.random() * 256);
         box.style.backgroundColor = `rgb(${random},${random},${random})`;
-    }))  
+    }))
 }
 
-function rainbowColor(){
+function rainbowColor() {
     const boxes = document.querySelectorAll(".box");
-    boxes.forEach(box => box.addEventListener("mouseover",() => {
+    boxes.forEach(box => box.addEventListener("mouseover", () => {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         box.style.backgroundColor = `rgb(${r},${g},${b})`;
-    }))  
+    }))
 }
 
 createGrid(16, 16);
